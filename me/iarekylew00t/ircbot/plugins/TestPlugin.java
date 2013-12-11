6a0daa197ff6b6839d9f0679b040af22ea2da9e3
Kyle Colantonio <kyle10468@gmail.com>
2013-12-10 20:09:21 -0500

    Added TestPlugin
    Used as an example

---
+++ me/iarekylew00t/ircbot/plugins/TestPlugin.java
@@ -0,0 +1,31 @@
+package me.iarekylew00t.ircbot.plugins;
+
+import java.util.HashMap;
+
+import org.pircbotx.hooks.events.MessageEvent;
+
+import me.iarekylew00t.ircbot.hooks.IRCPlugin;
+
+public class TestPlugin extends IRCPlugin {
+	private static String _NAME = "TestPlugin", _VER = "1.0.0";
+	private static HashMap _CMDS;
+	
+	public TestPlugin() {
+		super(_NAME, _VER, _CMDS);
+	}
+	
+	@Override
+	public void onEnable() {
+	}
+	
+	@Override
+	public void onDisable() {
+	}
+
+	@Override
+	public void onMessage(MessageEvent e) {
+		if (this.isEnabled() && e.getMessage().startsWith("$")) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
