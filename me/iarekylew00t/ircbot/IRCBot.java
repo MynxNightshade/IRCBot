70fd59574747c2751af2470414b1f40355230443
Kyle Colantonio <kyle10468@gmail.com>
2013-12-28 16:46:06 -0500

    Added slf4j and Logback

--- me/iarekylew00t/ircbot/IRCBot.java
+++ me/iarekylew00t/ircbot/IRCBot.java
@@ -2,9 +2,12 @@ package me.iarekylew00t.ircbot;
 
 import org.pircbotx.Configuration;
 import org.pircbotx.PircBotX;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class IRCBot extends PircBotX {
 	private static String _VER = "1.0.0";
+	private final Logger _LOG = LoggerFactory.getLogger(this.getClass()); //WIP
 
 	public IRCBot(Configuration configuration) {
 		super(configuration);
@@ -16,5 +19,9 @@ public class IRCBot extends PircBotX {
 	
 	public String getVersion() {
 		return _VER;
+	}
+	
+	public Logger getLogger() {
+		return this._LOG;
 	}
 }
