600b56e8181f94bcbed531ac915715ea7ef7a790
Kyle Colantonio <kyle10468@gmail.com>
2014-02-18 22:14:20 -0500

    Created PluginException

---
+++ me/iarekylew00t/ircbot/exceptions/PluginException.java
@@ -0,0 +1,21 @@
+package me.iarekylew00t.ircbot.exceptions;
+
+public class PluginException extends Exception {
+	private static final long serialVersionUID = -6550589076149552514L;
+
+	public PluginException() {
+		super();
+	}
+	
+	public PluginException(String message) {
+		super(message);
+	}
+	
+	public PluginException(Throwable cause) {
+		super(cause);
+	}
+	
+	public PluginException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
