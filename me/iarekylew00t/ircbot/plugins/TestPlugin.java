995869782be39535191153792be8f2bcd9336024
Kyle Colantonio <kyle10468@gmail.com>
2014-02-18 22:13:59 -0500

    Fixed Test Plugin
    This will purposely disable itself.

--- me/iarekylew00t/ircbot/plugins/TestPlugin.java
+++ me/iarekylew00t/ircbot/plugins/TestPlugin.java
@@ -7,16 +7,20 @@ import org.pircbotx.hooks.events.MessageEvent;
 import me.iarekylew00t.ircbot.hooks.IRCPlugin;
 
 public class TestPlugin extends IRCPlugin {
-	private static final String _NAME = "TestPlugin", _VER = "1.0.0";
-	private static HashMap _CMDS;
+	private static final String NAME = "TestPlugin", VER = "1.0.0";
+	private static HashMap<String, String> CMDS = new HashMap<String, String>();
+	static {
+		//Add Commands here
+		CMDS.put("test", "$test");
+	}
 	
 	public TestPlugin() {
-		super(_NAME, _VER, _CMDS);
+		super(NAME, VER, CMDS);
 	}
 	
 	@Override
 	public void onEnable() {
-		this.info("This is a test");
+		this.info("This is a test" + 1/0);
 	}
 	
 	@Override
