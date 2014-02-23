33100a04f6b367aeead3a423eafe8dac5ffc77e9
Kyle Colantonio <kyle10468@gmail.com>
2014-02-23 17:49:05 -0500

    Created a CommandListener

--- me/iarekylew00t/ircbot/event/listeners/CommandListener.java
+++ me/iarekylew00t/ircbot/event/listeners/CommandListener.java
@@ -1,31 +1,26 @@
 package me.iarekylew00t.ircbot.event.listeners;
 
-import me.iarekylew00t.ircbot.IRCBot;
-import me.iarekylew00t.ircbot.hooks.IRCCommand;
+import me.iarekylew00t.ircbot.hooks.Command;
 
 import org.pircbotx.hooks.ListenerAdapter;
 import org.pircbotx.hooks.events.MessageEvent;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class CommandListener extends ListenerAdapter{
-	private static Logger LOG;
-	
-	public CommandListener(IRCBot bot) {
-		LOG = bot.getLogger();
-	}
+	private final Logger LOG = LoggerFactory.getLogger(CommandListener.class);
 	
 	@Override
 	public void onMessage(MessageEvent e) {
 		if (e.getMessage().startsWith("$")) {
-			
-			IRCCommand cmd = new IRCCommand(e.getMessage());
+			Command cmd = new Command(e.getMessage());
 
 			if (cmd.hasArgs()) {
-				LOG.info(e.getUser().getNick() + " issued command: " + cmd.getCmd() + " '" + cmd.combineArgs() + "'");
+				this.LOG.info(e.getUser().getNick() + " issued command: " + cmd.getCmd() + " " + cmd.getArgs());
 			} else {
-				LOG.info(e.getUser().getNick() + " issued command: " + cmd.getCmd());
+				this.LOG.info(e.getUser().getNick() + " issued command: " + cmd.getCmd());
 			}
-			
+
 			if (!cmd.isValidCmd()) {
 				e.respond("'" + cmd.getCmd() + "' is not a valid command.");
 			}
