296bdd1151e6237407ee92fdad90eee95c23c2d3
Kyle Colantonio <kyle10468@gmail.com>
2014-02-23 21:27:50 -0500

    Moved src Files

---
+++ src/me/iarekylew00t/ircbot/IRCBotMain.java
@@ -0,0 +1,49 @@
+package me.iarekylew00t.ircbot;
+
+import java.nio.charset.Charset;
+
+import org.pircbotx.Configuration;
+
+import me.iarekylew00t.ircbot.event.listeners.CommandListener;
+import me.iarekylew00t.ircbot.plugins.*;
+
+public class IRCBotMain {
+	private static IRCBot BOT;
+	
+	public static void main(String[] args) throws Exception {
+		IRCConfiguration config = new IRCConfiguration();
+		if (config.firstTimeLoad()) {
+			config.setProp("nick", "IRCBot");
+			config.setProp("nickPass", "");
+			config.setProp("login", "PircBotX");
+			config.setProp("server", "irc.esper.net");
+			config.setProp("serverPass", "");
+			config.setProp("port", "6667");
+			config.setProp("channel", "#channel1,#channel2,#channel3");
+			config.setProp("identServ", "false");
+			config.update();
+		}
+		Configuration botConfig = new Configuration.Builder()
+			/* Hardcoded Settings */
+			.setEncoding(Charset.forName("UTF-8"))
+			.setAutoNickChange(true)
+			.setAutoReconnect(true)
+			/* Bot Settings */
+			.setLogin(config.getProp("login"))
+			.setName(config.getProp("nick"))
+			.setNickservPassword(!config.getProp("nickPass").isEmpty() ? config.getProp("nickPass") : null)
+			.setServer(config.getProp("server"), Integer.parseInt(config.getProp("port")), config.getProp("serverPass"))
+			.addAutoJoinChannel(config.getProp("channel"))
+			/* Built-in Listeners */
+			.addListener(new CommandListener())
+			.buildConfiguration();
+		BOT = new IRCBot(botConfig);
+		BOT.setVersion("2.1.0_beta");
+		BOT.getLogger().info("===== STARTING ARADIABOT V" + BOT.getVersion() + " =====");
+		
+		/* BEGIN PLUGINS */
+		BOT.addPlugin(new TestPlugin(BOT));
+		
+		BOT.startBot(); //transformers roll out
+	}
+}
--- src/me/iarekylew00t/ircbot/IRCConfiguration.java
+++ src/me/iarekylew00t/ircbot/IRCConfiguration.java
@@ -1,4 +1,4 @@
-package me.iarekylew00t.utils;
+package me.iarekylew00t.ircbot;
 
 import java.io.File;
 import java.io.FileInputStream;
