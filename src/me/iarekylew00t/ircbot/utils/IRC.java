d174720905e23c7134fc370b8073c460df19a63a
Kyle Colantonio <kyle10468@gmail.com>
2014-02-24 23:13:44 -0500

    Created IRC class
    Used for Command Permissions

---
+++ src/me/iarekylew00t/ircbot/utils/IRC.java
@@ -0,0 +1,11 @@
+package me.iarekylew00t.ircbot.utils;
+
+public final class IRC {
+	public static final int NORMAL		= 0;
+	public static final int VOICE		= 1;
+	public static final int HALF_OP		= 2;
+	public static final int OP			= 3;
+	public static final int SUPER_OP	= 4;
+	public static final int OWNER		= 5;
+	public static final int IRC_OP		= 6;
+}
